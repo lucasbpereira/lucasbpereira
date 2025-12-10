5414aada62bab41a0d3dcfeb3b70d6bfd98d9930
Lucas Barbosa <lucasbpereira@outlook.com>
2025-12-10 11:49:11 -0300

    ajustes no header

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -13,7 +13,8 @@ export class Home implements AfterViewInit {
 
   logo: string = '{lucasbpereira}'
   timeline!: gsap.core.Timeline;
-  timelineHeader!: gsap.core.Timeline;
+  timelineHeader: gsap.core.Timeline | null = null;
+  headerAnimationPlayed: boolean = false;
 
   ngAfterViewInit() {
     gsap.registerPlugin(ScrollTrigger);
@@ -28,24 +29,55 @@ export class Home implements AfterViewInit {
       }
     });
 
-    this.timelineHeader = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".logo-box",
-        start: "top top",
-        markers: false
-      }
-    });
-
     this.timeline.fromTo(".mask-overlay .logo",
       { scale: 200, opacity: 1 },
       { scale: 1, opacity: 1, duration: 1, ease: "power2.inOut" }
     );
 
-    this.timeline.to(".mask-overlay .logo", { opacity: 0, duration: 0.2 }, "-=0.1");
-    this.timeline.to(".header-logo", { opacity: 1, color: '#fff', duration: 0.2 }, "<");
-    this.timeline.to(".fixed-header", {position:"fixed", top: 0, left: 0, zIndex: 25, duration: 0.2 }, "-=0.1");
-    this.timeline.to(".fixed-header .title", {top: 0,  zIndex: 25, duration: 0.2 }, "-=0.1");
-    this.timeline.to(".fixed-header .subtitle", {left: 0,  zIndex: 25, duration: 0.2 }, "-=0.1");
-    this.timeline.to(".fixed-header .box-image", {bottom: 0,  zIndex: 25, duration: 0.2 }, "-=0.1");
+    this.timeline.to(".mask-overlay .logo", { opacity: 0, duration: 0.2 }, "-=0.1")
+      .to(".header-logo", { opacity: 1, color: '#fff', duration: 0.2 }, "<")
+      .to(".fixed-header", {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 25,
+        duration: 0.2,
+        markers: true,
+        onComplete: () => {
+          this.playHeaderAnimation();
+          console.log('Header animation triggered');
+        }
+      }, "-=0.1")
+      .addLabel("headerFixed");
+  }
+
+  playHeaderAnimation() {
+    console.log('Playing header animation');
+
+    // Cria e executa a timeline do header
+    this.timelineHeader = gsap.timeline();
+
+    this.timelineHeader
+      .to(".fixed-header .title", {
+        top: 0,
+        zIndex: 25,
+        duration: 0.2,
+        opacity:1,
+        ease: "power2.out"
+      })
+      .to(".fixed-header .subtitle", {
+        left: 0,
+        zIndex: 25,
+        duration: 0.2,
+        opacity:1,
+        ease: "power2.out"
+      })
+      .to(".fixed-header .box-image", {
+        bottom: 0,
+        zIndex: 25,
+        duration: 0.2,
+        opacity:1,
+        ease: "power2.out"
+      });
   }
 }
