f07f1aabad3a93612b30b91f7cda71b1f1268ae5
Lucas Barbosa <lucasbpereira@outlook.com>
2025-12-12 09:24:07 -0300

    menu iniciado

---
+++ src/app/components/menu/menu.ts
@@ -0,0 +1,76 @@
+import { AfterViewInit, Component, signal } from '@angular/core';
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss',
+})
+export class Menu implements AfterViewInit {
+  isOpen = signal(false);
+  tl!: gsap.core.Timeline;
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(SplitText); // Important for text animations
+
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+
+    let isOpen = false; // Hamburger open state
+
+    this.tl = gsap.timeline({ paused: true });
+
+    this.tl.to("#top-line", { // Top line rotate and move down
+        rotate: 45,
+        y: 3.5,
+        duration: 1,
+        ease: "expo.out"
+      }, 0) // Starts immediately after click
+      .to("#bottom-line", { // Bottom line rotate and move up
+        rotate: -45,
+        y: -3.5,
+        duration: 1,
+        ease: "expo.out", // Fast start, very smooth end
+      }, "<")
+      .to("#nav", { // Menu slide in (right to left)
+        x: "0%", // Goes from initial position to 0% (on-screen)
+        duration: 1,
+        ease: "expo.out"
+      }, "<")
+      .from("#links-section", { // Links slide in (right to left)
+        x: 100,
+        autoAlpha: 0,
+        duration: 1,
+        ease: "back.out", // "Bounce"
+      }, "<0.3"); // Starts 0.3 seconds after previous animation
+
+
+    // document.fonts.ready.then(() => {	// Waits for fonts to be loaded before start
+    //   SplitText.create("#click-menu", {
+    //     type: "chars", // Splits words or sentences into characters
+    //     autoSplit: true,
+    //     onSplit: (el) => {
+    //       gsap.to(el.chars, {
+    //         color: "silver",
+    //         autoAplha: true,
+    //         duration: 1,
+    //         stagger: { amount: 2, repeat: -1, yoyo: true } // "Pulse" effect
+    //       })
+    //     }
+    //   });
+    // });
+  }
+
+
+  toggleMenu() {
+
+    if (this.isOpen()) {
+      this.tl.reverse(); // Plays animations in reverse when closing menu
+    } else {
+      this.tl.play(0); // Plays animation
+    }
+
+    this.isOpen.update(isOpen => !isOpen);
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,13 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+import { Menu } from '../../components/menu/menu';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Menu],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -13,11 +16,10 @@ export class Home implements AfterViewInit {
 
   logo: string = '{lucasbpereira}'
   timeline!: gsap.core.Timeline;
-  timelineHeader: gsap.core.Timeline | null = null;
-  headerAnimationPlayed: boolean = false;
+  timelineHeader!: gsap.core.Timeline;
 
   ngAfterViewInit() {
-    gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrambleTextPlugin);
 
     this.timeline = gsap.timeline({
       scrollTrigger: {
@@ -30,54 +32,49 @@ export class Home implements AfterViewInit {
     });
 
     this.timeline.fromTo(".mask-overlay .logo",
-      { scale: 200, opacity: 1 },
+      { scale: 250, opacity: 1 },
       { scale: 1, opacity: 1, duration: 1, ease: "power2.inOut" }
     );
 
-    this.timeline.to(".mask-overlay .logo", { opacity: 0, duration: 0.2 }, "-=0.1")
-      .to(".header-logo", { opacity: 1, color: '#fff', duration: 0.2 }, "<")
-      .to(".fixed-header", {
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: 25,
-        duration: 0.2,
-        markers: true,
-        onComplete: () => {
-          this.playHeaderAnimation();
-          console.log('Header animation triggered');
-        }
+    this.timeline
+      .to(".mask-overlay .logo", {
+          opacity: 0,
+          duration: 0.2
       }, "-=0.1")
-      .addLabel("headerFixed");
-  }
-
-  playHeaderAnimation() {
-    console.log('Playing header animation');
-
-    // Cria e executa a timeline do header
-    this.timelineHeader = gsap.timeline();
-
-    this.timelineHeader
-      .to(".fixed-header .title", {
-        top: 0,
-        zIndex: 25,
-        duration: 0.2,
-        opacity:1,
-        ease: "power2.out"
+      .fromTo(".header-logo",
+          { opacity: 1, color: '#000' },
+          { opacity: 1, color: '#fff', duration: 0.2 },
+      "<")
+      .to(".fixed-header", {
+          zIndex: 25,
+          duration: 0.2
+      }, "-=0.1")
+      .set(".mask-overlay", {
+          mixBlendMode: "normal"
       })
-      .to(".fixed-header .subtitle", {
-        left: 0,
-        zIndex: 25,
-        duration: 0.2,
-        opacity:1,
-        ease: "power2.out"
-      })
-      .to(".fixed-header .box-image", {
-        bottom: 0,
-        zIndex: 25,
-        duration: 0.2,
-        opacity:1,
-        ease: "power2.out"
-      });
+      .fromTo(".container",
+          { opacity: 0 },
+          { opacity: 1, duration: 0.2 },
+      "-=0.1")
+      .fromTo(".mask-overlay .title",
+          { top: -50, opacity: 0 },
+          { top: 0, opacity: 1, duration: 0.2 },
+      "+=0.1")
+      .fromTo(".mask-overlay .box-text",
+          { left: -50, opacity: 0 },
+          { left: 0, opacity: 1, duration: 0.2 },
+      "+=0.1")
+      .to(".myName",{
+        scrambleText: {
+          text: "LUCAS BARBOSA PEREIRA",
+          chars: "upperCase",
+          speed: 0.3
+        },
+        duration: 2
+      }, "+=0.1")
+      .fromTo(".mask-overlay .box-image",
+          { bottom: -50, opacity: 0 },
+          { bottom: 0, opacity: 1, duration: 0.2 },
+      "+=0.1");
   }
 }
